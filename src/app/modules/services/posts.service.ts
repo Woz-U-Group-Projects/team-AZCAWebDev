3449ae1b9d9895525236f8c9481b514393ed8a48
jcshelley <azshelleyfamily@cox.net>
2019-02-21 17:01:09 -0700

    Added express router to backend and updated routing on frontend

--- src/app/modules/services/posts.service.ts
+++ src/app/modules/services/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 import { Post } from '../../models/post';
 
@@ -12,7 +13,7 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   getPosts() {
     this.http
@@ -51,6 +52,7 @@ export class PostsService {
       post.id = id;
       this.posts.push(post);
       this.postsUpdated.next([...this.posts]);
+      this.router.navigate(['/']);
     });
   }
 
@@ -64,6 +66,7 @@ export class PostsService {
           updatedPosts[oldPostIndex] = post;
           this.posts = updatedPosts;
           this.postsUpdated.next([...this.posts]);
+          this.router.navigate(['/']);
       });
   }
 
